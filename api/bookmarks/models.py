be7983108aaefbdcd4a4596a1b0653106cad4cfc
Jeff Hutchison <jeff@jeffhutchison.com>
2021-02-02 16:25:56 -0500

    Add bookmarks model

--- api/bookmarks/models.py
+++ api/bookmarks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
