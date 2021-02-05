0188f9df6126f4867f84c2c14e17b489c3e711b7
Jeff Hutchison <jeff@jeffhutchison.com>
2021-02-05 16:18:09 -0500

    Add JWT auth

--- api/puka/schema.py
+++ api/puka/schema.py
@@ -1,10 +1,18 @@
 import graphene
+import graphql_jwt
 
 import bookmarks.schema
+import users.schema
 
 
-class Query(bookmarks.schema.Query, graphene.ObjectType):
+class Query(bookmarks.schema.Query, users.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(users.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- api/puka/settings.py
+++ api/puka/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
 ]
 
 ROOT_URLCONF = "puka.urls"
@@ -128,4 +129,14 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-GRAPHENE = {"SCHEMA": "puka.schema.schema"}
+GRAPHENE = {
+    "SCHEMA": "puka.schema.schema",
+    "MIDDLEWARE": [
+        "graphql_jwt.middleware.JSONWebTokenMiddleware",
+    ],
+}
+
+AUTHENTICATION_BACKENDS = [
+    "graphql_jwt.backends.JSONWebTokenBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
