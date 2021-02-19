fca65a2d30129f0beddcf97a05716b09bf61392a
Jeff Hutchison <jeff@jeffhutchison.com>
2021-02-19 11:52:51 -0500

    Update Heroku config

--- puka/settings.py
+++ puka/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -150,3 +152,6 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
