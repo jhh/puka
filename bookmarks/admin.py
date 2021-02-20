3db708526cb16de0ad9ec9c8b6c8ddd56ac68a1e
Jeff Hutchison <jeff@jeffhutchison.com>
2021-02-20 11:59:18 -0500

    Add bookmarks to admin

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bookmark
+
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "url",
+        "tags",
+        "created_at",
+        "title_description_search",
+    )
+    list_filter = ("created_at",)
+    date_hierarchy = "created_at"
