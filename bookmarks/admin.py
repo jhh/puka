669a74d8b036b8a31e109ed29c7a16870edbf7dd
Jeff Hutchison <jeff@jeffhutchison.com>
2021-02-20 12:33:12 -0500

    Update bookmark admin

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -8,11 +8,10 @@ class BookmarkAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "title",
-        "description",
         "url",
         "tags",
         "created_at",
-        "title_description_search",
     )
     list_filter = ("created_at",)
     date_hierarchy = "created_at"
+    exclude = ("title_description_search",)
