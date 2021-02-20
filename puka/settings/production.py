bd712a6483d3f36be9de34c485da76b98dfa30e7
Jeff Hutchison <jeff@jeffhutchison.com>
2021-02-20 10:40:31 -0500

    Remove django_heroku dep

--- puka/settings/production.py
+++ puka/settings/production.py
@@ -1,7 +1,6 @@
 """
 Django settings for Puka production deployments.
 """
-import django_heroku
 from puka.settings.common import *
 
 SECRET_KEY = os.environ["SECRET_KEY"]
@@ -11,9 +10,43 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-django_heroku.settings(locals(), secret_key=False, databases=False)
+ALLOWED_HOSTS = ["*"]
 
-# Place static in the same location as webpack build files
+DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
+
 STATIC_ROOT = os.path.join(BASE_DIR, "build", "static")
 STATICFILES_DIRS = []
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": (
+                "%(asctime)s [%(process)d] [%(levelname)s] "
+                + "pathname=%(pathname)s lineno=%(lineno)s "
+                + "funcname=%(funcName)s %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "null": {
+            "level": "DEBUG",
+            "class": "logging.NullHandler",
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "testlogger": {
+            "handlers": ["console"],
+            "level": "INFO",
+        }
+    },
+}
