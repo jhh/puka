3a0adf59cd8f2c4a57327c5d3ae635dd1d77bdb4
Jeff Hutchison <jeff@jeffhutchison.com>
2021-02-20 09:33:48 -0500

    Update prod settings

--- puka/settings/production.py
+++ puka/settings/production.py
@@ -4,14 +4,14 @@ Django settings for Puka production deployments.
 import django_heroku
 from puka.settings.common import *
 
-SECRET_KEY = os.environ["PUKA_SECRET_KEY"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = False
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), secret_key=False, databases=False)
 
 # Place static in the same location as webpack build files
 STATIC_ROOT = os.path.join(BASE_DIR, "build", "static")
