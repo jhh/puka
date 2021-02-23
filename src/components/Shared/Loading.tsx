c3af7d3cc5dbb73b4d9d4129f573747c70d74e55
Jeff Hutchison <jeff@jeffhutchison.com>
2021-02-23 08:33:32 -0500

    Add loading spinner

--- src/components/Shared/Loading.tsx
+++ src/components/Shared/Loading.tsx
@@ -1,3 +1,26 @@
-const Login = () => <div>Loading</div>;
+import React from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default Login;
+const Loading = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CircularProgress className={classes.progress} />
+    </div>
+  );
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100vw",
+    textAlign: "center",
+  },
+  progress: {
+    margin: theme.spacing(2),
+    color: theme.palette.secondary.dark,
+  },
+}));
+
+export default Loading;
