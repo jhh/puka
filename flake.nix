f51e6649d8784ade9da61bc08b06ac22a6782f83
Jeff Hutchison <jeff@jeffhutchison.com>
2022-05-14 10:47:46 -0400

    Add flake (#1)
    * Add flake
    
    * Don't use HTTPS
    
    * Fix build
    
    * Remove old env vars
    
    * Add env file
    
    * Add nginx proxy

---
+++ flake.nix
@@ -0,0 +1,121 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    let
+      out = system:
+        let pkgs = nixpkgs.legacyPackages."${system}";
+        in
+        {
+
+          devShell = pkgs.mkShell {
+
+            nativeBuildInputs = with pkgs; [
+              postgresql
+              yarn
+              python3Packages.poetry
+              nodejs-16_x
+            ];
+
+            buildInputs = pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.openssl;
+          };
+
+          packages.default = let puka = with pkgs.poetry2nix; mkPoetryApplication {
+            projectDir = ./.;
+            preferWheels = true;
+
+            fixupPhase = ''
+              tar -xzf build.tgz
+              export SECRET_KEY=dummy
+              export DJANGO_SETTINGS_MODULE=puka.settings.production
+              python manage.py collectstatic --no-input
+              cp -vfr build $out/lib/python3.9/site-packages/
+            '';
+          }; in puka.dependencyEnv;
+
+        };
+    in
+    with utils.lib; eachSystem defaultSystems out // {
+
+      nixosModules.default = { config, lib, pkgs, ... }:
+        with lib;
+        let
+          cfg = config.j3ff.puka;
+        in
+        {
+          options.j3ff.puka = {
+            enable = mkEnableOption "Enable the Puka service";
+          };
+
+          config = mkIf cfg.enable
+            {
+              # systemd.timers.dyndns = {
+              #   description = "Periodic update of home.j3ff.io DNS record";
+              #   wantedBy = [ "multi-user.target" ];
+
+              #   timerConfig = {
+              #     OnBootSec = "15min";
+              #     OnUnitActiveSec = "4h"
+              #   };
+              # };
+
+              systemd.services.puka = {
+                description = "Puka bookmarks";
+
+                wantedBy = [ "multi-user.target" ];
+
+                environment = {
+                  DJANGO_SETTINGS_MODULE = "puka.settings.production";
+                };
+
+                serviceConfig =
+                  let pkg = self.packages.${pkgs.system}.default;
+                  in
+                  {
+                    # agenix secret in github:jhh/nixos-configs
+                    EnvironmentFile = "/run/agenix/puka_secrets";
+                    ExecStart = "${pkg}/bin/gunicorn puka.wsgi --log-file -";
+
+                    DynamicUser = true;
+                    NoNewPrivileges = true;
+                    ProtectSystem = "strict";
+                  };
+              };
+
+              services.nginx = {
+                enable = true;
+                recommendedProxySettings = true;
+                virtualHosts."puka.j3ff.io" = {
+                  default = true;
+
+                  locations = {
+                    "/" = {
+                      proxyPass = "http://127.0.0.1:8000";
+                    };
+                  };
+                };
+              };
+            };
+        };
+
+
+
+      nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          self.nixosModules.default
+          ({ config, pkgs, ... }: {
+            # Only allow this to boot as a container
+            boot.isContainer = true;
+            networking.hostName = "puka";
+
+            j3ff.puka.enable = true;
+          })
+        ];
+      };
+    };
+
+}
