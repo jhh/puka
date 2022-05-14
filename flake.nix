7a63efea25aee96300633c789ff72bfe958b1d1c
Jeff Hutchison <jeff@jeffhutchison.com>
2022-05-14 08:35:57 -0400

    Fix build

--- flake.nix
+++ flake.nix
@@ -5,32 +5,119 @@
   };
 
   outputs = { self, nixpkgs, utils }:
-    let out = system:
-      let pkgs = nixpkgs.legacyPackages."${system}";
-      in
-      {
+    let
+      out = system:
+        let pkgs = nixpkgs.legacyPackages."${system}";
+        in
+        {
 
-        devShell = pkgs.mkShell {
+          devShell = pkgs.mkShell {
 
-          nativeBuildInputs = with pkgs; [
-            postgresql
-            yarn
-            python3Packages.poetry
-            nodejs-16_x
-          ];
+            nativeBuildInputs = with pkgs; [
+              postgresql
+              yarn
+              python3Packages.poetry
+              nodejs-16_x
+            ];
 
-          buildInputs = pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.openssl;
+            buildInputs = pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.openssl;
+          };
+
+          packages.default = let puka = with pkgs.poetry2nix; mkPoetryApplication {
+            projectDir = ./.;
+            preferWheels = true;
+
+            fixupPhase = ''
+              tar -xzf build.tgz
+              export SECRET_KEY=dummy
+              export DJANGO_SETTINGS_MODULE=puka.settings.production
+              python manage.py collectstatic --no-input
+              cp -vfr build $out/lib/python3.9/site-packages/
+            '';
+          }; in puka.dependencyEnv;
+
+          # packages.default = with pkgs.poetry2nix; mkPoetryApplication {
+          #   projectDir = ./.;
+          #   preferWheels = true;
+
+          #   fixupPhase = ''
+          #     tar -xzf build.tgz
+          #     export SECRET_KEY=dummy
+          #     export DJANGO_SETTINGS_MODULE=puka.settings.production
+          #     python manage.py collectstatic --no-input
+          #     cp -vfr build $out
+          #   '';
+          # };
+
+        };
+    in
+    with utils.lib; eachSystem defaultSystems out // {
+
+      nixosModules.default = { config, lib, pkgs, ... }:
+        with lib;
+        let
+          cfg = config.j3ff.puka;
+        in
+        {
+          options.j3ff.puka = {
+            enable = mkEnableOption "Enable the Puka service";
+          };
+
+          config = mkIf cfg.enable
+            {
+              # systemd.timers.dyndns = {
+              #   description = "Periodic update of home.j3ff.io DNS record";
+              #   wantedBy = [ "multi-user.target" ];
+
+              #   timerConfig = {
+              #     OnBootSec = "15min";
+              #     OnUnitActiveSec = "4h"
+              #   };
+              # };
+
+              systemd.services.puka = {
+                description = "Puka bookmarks";
+
+                wantedBy = [ "multi-user.target" ];
+
+                environment = {
+                  DATABASE_URL = "postgres://puka:PuaExJXjZDNv4PqBcZmb@192.168.1.46:5432/puka";
+                  SECRET_KEY = "gy=mqm_f*98ghz2zq$*uq%v1!!n!!b0u995$)=7-4q!!*3tt-q";
+                  DJANGO_SETTINGS_MODULE = "puka.settings.production";
+                };
+
+                serviceConfig =
+                  let pkg = self.packages.${pkgs.system}.default;
+                  in
+                  {
+                    # Type = "oneshot";
+                    # agenix secret in github:jhh/nixos-configs
+                    # LoadCredential = "AWS_SECRET_ACCESS_KEY:/run/agenix/aws_secret";
+                    ExecStart = "${pkg}/bin/gunicorn puka.wsgi --log-file -";
+
+                    DynamicUser = true;
+                    NoNewPrivileges = true;
+                    ProtectSystem = "strict";
+                  };
+              };
+            };
         };
 
-        defaultPackage = with pkgs.poetry2nix; mkPoetryApplication {
-          projectDir = ./.;
-          preferWheels = true;
-        };
 
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
 
-      }; in with utils.lib; eachSystem defaultSystems out;
+      nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          self.nixosModules.default
+          ({ config, pkgs, ... }: {
+            # Only allow this to boot as a container
+            boot.isContainer = true;
+            networking.hostName = "puka";
+
+            j3ff.puka.enable = true;
+          })
+        ];
+      };
+    };
 
 }
