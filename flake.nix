9557d2b544cbcb66af8d2690aa7a25e325cc878e
Jeff Hutchison <jeff@jeffhutchison.com>
2022-09-27 13:16:30 -0400

    Fix uwsgi path

--- flake.nix
+++ flake.nix
@@ -96,7 +96,7 @@
                 {
                   # agenix secret in github:jhh/nixos-configs
                   EnvironmentFile = "/run/agenix/puka_secrets";
-                  ExecStart = "${pkgs.uwsgi}/bin/uwsgi --http :8000 --module config.wsgi";
+                  ExecStart = "${pkg}/bin/uwsgi --http :8000 --module config.wsgi";
 
                   Type = "notify";
                   NotifyAccess = "all";
