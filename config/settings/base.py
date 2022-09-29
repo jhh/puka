a76b2cdee514a1a0e9304129bfb824ff25369b3a
Jeff Hutchison <jeff@jeffhutchison.com>
2022-09-29 15:17:05 -0400

    Add development scripts

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,6 +44,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "puka.core.context_processors.tailwind_css",
             ],
         },
     },
