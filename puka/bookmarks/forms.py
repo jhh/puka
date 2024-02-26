2fa48cb4e30b0fc10b333c75ab3f423fa7a3e5d6
Jeff Hutchison <jeff@jeffhutchison.com>
2024-02-26 11:15:16 -0500

    Enable edit of active flag

--- puka/bookmarks/forms.py
+++ puka/bookmarks/forms.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from django.forms import CheckboxInput
 from django.forms import ModelForm
 from django.forms import Textarea
 from django.forms import TextInput
@@ -11,10 +12,11 @@ from .models import Bookmark
 class BookmarkForm(ModelForm):
     class Meta:
         model = Bookmark
-        fields = ["title", "description", "url", "tags"]
+        fields = ["title", "description", "url", "tags", "active"]
         widgets = {
             "title": TextInput(attrs={"class": "dj-input-text"}),
             "description": Textarea(attrs={"class": "dj-input-textarea"}),
             "url": URLInput(attrs={"class": "dj-input-url"}),
             "tags": TextInput(attrs={"class": "dj-input-text"}),
+            "active": CheckboxInput(attrs={"class": "hidden"}),
         }
