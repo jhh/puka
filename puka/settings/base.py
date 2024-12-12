3d1f76a240e965dd8ad6a1daac1dd07e77e60e17
Jeff Hutchison <jeff@jeffhutchison.com>
2024-12-12 14:45:06 -0500

    Fix DJANGO_STATIC_ROOT name

--- puka/settings/base.py
+++ puka/settings/base.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # STATIC_ROOT is where collectstatic will collect files for deployment
-STATIC_ROOT = os.environ.get("STATIC_ROOT", BASE_DIR / "static")
+STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", BASE_DIR / "static")
 
 # STATICFILES_DIR is where "django.contrib.staticfiles" looks during development
 STATICFILES_DIRS = [str(APPS_DIR / "static")]
