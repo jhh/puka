ad455058b438b9f6b4a261f5b306363bc10a54cb
Jeff Hutchison <jeff@jeffhutchison.com>
2024-12-19 11:43:34 -0500

    Add django-filter and crispy_tailwind

--- lib/static.nix
+++ lib/static.nix
@@ -4,15 +4,24 @@
   venv,
 }:
 let
+  baseCss = "puka/static/puka/base.css";
+  templates = "lib/python3.12/site-packages/crispy_tailwind/templates/tailwind";
+
   pukaCssJs = pkgs.buildNpmPackage {
     name = "django-static-deps";
     src = ../.;
-    npmDepsHash = "sha256-5betlQyJXp0zB15wdGu4oF5NisajKuH/e54k2n5uSyk=";
+    npmDepsHash = "sha256-JiWc6gmRf2Z6dPrXNuD9+sleSKsmuz9J5FSE5vA/ejI=";
     dontNpmBuild = true;
+
+    patchPhase = ''
+      runHook prePatch
+      substituteInPlace ${baseCss} --replace-fail "../../../.venv/${templates}" "${venv}/${templates}"
+      runHook postPatch
+    '';
 
     buildPhase = ''
       runHook preBuild
-      npx @tailwindcss/cli --minify --input=puka/static/puka/base.css --output=$out/puka/main.css
+      npx @tailwindcss/cli --minify --input=${baseCss} --output=$out/puka/main.css
       npx esbuild --bundle --minify --outfile=$out/puka/main.js puka/static/puka/base.js
       runHook postBuild
     '';
--- puka/bookmarks/urls.py
+++ puka/bookmarks/urls.py
@@ -1,15 +1,21 @@
 from __future__ import annotations
 
-from django.urls import path
-from django.urls import URLPattern
-from django.urls import URLResolver
+from django.urls import URLPattern, URLResolver, path
+from django_filters.views import FilterView
 
-from .views import bookmark_create
-from .views import bookmark_update
-from .views import bookmarks
+from .models import Bookmark
+from .views import bookmark_create, bookmark_update, bookmarks
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("", bookmarks, name="bookmarks"),
+    path(
+        "filter/",
+        FilterView.as_view(
+            model=Bookmark,
+            filterset_fields=["title", "url", "active", "created"],
+        ),
+        name="bookmark-list",
+    ),
     path("new/", bookmark_create, name="bookmark-create"),
     path("edit/<int:pk>/", bookmark_update, name="bookmark-update"),
 ]
