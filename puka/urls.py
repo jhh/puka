1088801fee0a480a544206d210af2cae7c0a0d7c
Jeff Hutchison <jeff@jeffhutchison.com>
2024-12-24 14:14:12 -0500

    Implement sidebar navigation

--- puka/urls.py
+++ puka/urls.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 from django.conf import settings
 from django.contrib import admin
 from django.urls import URLPattern, URLResolver, include, path
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("", RedirectView.as_view(url="/bookmarks/", permanent=False)),
     path("bookmarks/", include("puka.bookmarks.urls")),
+    path("upkeep/", TemplateView.as_view(template_name="sidebar.html"), name="upkeep"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("users/", include("puka.users.urls")),
     path("admin/", admin.site.urls),
