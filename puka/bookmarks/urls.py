f55ac06f7d94ee52a6ed554b771bdcf54d5425c7
Jeff Hutchison <jeff@jeffhutchison.com>
2024-12-25 17:11:20 -0500

    Add url namespace to bookmarks

--- puka/bookmarks/urls.py
+++ puka/bookmarks/urls.py
@@ -12,12 +12,13 @@ from .views import (
     tags_list,
 )
 
+app_name = "bookmarks"
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("", bookmarks, name="bookmarks"),
-    path("<int:pk>/", bookmark_detail, name="bookmark-detail"),
-    path("new/", bookmark_new, name="bookmark-new"),
-    path("<int:pk>/edit/", bookmark_edit, name="bookmark-edit"),
-    path("<int:pk>/delete/", bookmark_delete, name="bookmark-delete"),
-    path("filter/", bookmarks_filter, name="bookmark-filter"),
-    path("tags/", tags_list, name="bookmark-tags"),
+    path("", bookmarks, name="list"),
+    path("<int:pk>/", bookmark_detail, name="detail"),
+    path("new/", bookmark_new, name="new"),
+    path("<int:pk>/edit/", bookmark_edit, name="edit"),
+    path("<int:pk>/delete/", bookmark_delete, name="delete"),
+    path("filter/", bookmarks_filter, name="filter"),
+    path("tags/", tags_list, name="tags"),
 ]
