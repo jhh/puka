5d900fb7edb89fb1b2d0fe19a276b9406f9a2215
Jeff Hutchison <jeff@jeffhutchison.com>
2024-12-25 11:17:59 -0500

    Complete migration squash

--- puka/bookmarks/migrations/0003_alter_bookmark_options_and_more_squashed_0009_alter_bookmark_options.py
+++ puka/bookmarks/migrations/0003_alter_bookmark_options_and_more_squashed_0009_alter_bookmark_options.py
@@ -6,53 +6,55 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
-    replaces = [('bookmarks', '0003_alter_bookmark_options_and_more'), ('bookmarks', '0004_bookmark_active'), ('bookmarks', '0005_bookmark_bookmarks_b_created_9a00a6_idx'), ('bookmarks', '0006_alter_bookmark_url'), ('bookmarks', '0007_remove_bookmark_bookmarks_b_tags_65d16c_gin_and_more'), ('bookmarks', '0008_bookmark_tags'), ('bookmarks', '0009_alter_bookmark_options')]
-
     dependencies = [
-        ('bookmarks', '0002_title_description_search_trigger'),
-        ('taggit', '0006_rename_taggeditem_content_type_object_id_taggit_tagg_content_8fc721_idx'),
+        ("bookmarks", "0002_title_description_search_trigger"),
+        ("taggit", "0006_rename_taggeditem_content_type_object_id_taggit_tagg_content_8fc721_idx"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='bookmark',
-            options={'ordering': ['-created']},
+            name="bookmark",
+            options={"ordering": ["-created"]},
         ),
         migrations.AlterField(
-            model_name='bookmark',
-            name='title_description_search',
+            model_name="bookmark",
+            name="title_description_search",
             field=django.contrib.postgres.search.SearchVectorField(editable=False, null=True),
         ),
         migrations.AddField(
-            model_name='bookmark',
-            name='active',
+            model_name="bookmark",
+            name="active",
             field=models.BooleanField(default=True),
         ),
         migrations.AddIndex(
-            model_name='bookmark',
-            index=models.Index(fields=['-created'], name='bookmarks_b_created_9a00a6_idx'),
+            model_name="bookmark",
+            index=models.Index(fields=["-created"], name="bookmarks_b_created_9a00a6_idx"),
         ),
         migrations.AlterField(
-            model_name='bookmark',
-            name='url',
+            model_name="bookmark",
+            name="url",
             field=models.URLField(max_length=500, unique=True),
         ),
         migrations.RemoveIndex(
-            model_name='bookmark',
-            name='bookmarks_b_tags_65d16c_gin',
+            model_name="bookmark",
+            name="bookmarks_b_tags_65d16c_gin",
         ),
         migrations.RemoveField(
-            model_name='bookmark',
-            name='tags',
+            model_name="bookmark",
+            name="tags",
         ),
         migrations.AddField(
-            model_name='bookmark',
-            name='tags',
-            field=taggit.managers.TaggableManager(help_text='A comma-separated list of tags.', through='taggit.TaggedItem', to='taggit.Tag', verbose_name='Tags'),
+            model_name="bookmark",
+            name="tags",
+            field=taggit.managers.TaggableManager(
+                help_text="A comma-separated list of tags.",
+                through="taggit.TaggedItem",
+                to="taggit.Tag",
+                verbose_name="Tags",
+            ),
         ),
         migrations.AlterModelOptions(
-            name='bookmark',
-            options={'ordering': ('-created',)},
+            name="bookmark",
+            options={"ordering": ("-created",)},
         ),
     ]
