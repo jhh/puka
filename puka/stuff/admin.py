dee2f5ed9069933ea05678f534bf3898f251e055
Jeff Hutchison <jeff@jeffhutchison.com>
2025-02-10 07:18:37 -0500

    Add treebeard admin for Location

--- puka/stuff/admin.py
+++ puka/stuff/admin.py
@@ -2,10 +2,17 @@
 from __future__ import annotations
 
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
 from puka.stuff.models import Category, InventoryTransaction, Location, Product
 
+
+@admin.register(Location)
+class LocationAdmin(TreeAdmin):
+    form = movenodeform_factory(Location)
+
+
 admin.site.register(Category)
-admin.site.register(Location)
 admin.site.register(Product)
 admin.site.register(InventoryTransaction)
