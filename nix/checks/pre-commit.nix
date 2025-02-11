4dbc8dedf842cf4d0910237d8c057a3cd5e35a12
Jeff Hutchison <jeff@jeffhutchison.com>
2025-02-11 14:57:07 -0500

    Add check-toml pre-commit check

--- nix/checks/pre-commit.nix
+++ nix/checks/pre-commit.nix
@@ -26,6 +26,7 @@ inputs.pre-commit-hooks.lib.${system}.run {
       check-added-large-files.args = [ "--maxkb=25" ];
       check-case-conflicts.enable = true;
       check-json.enable = true;
+      check-toml.enable = true;
       pyupgrade.enable = true;
       pyupgrade.args = [ "--py312-plus" ];
       add-trailing-comma = {
