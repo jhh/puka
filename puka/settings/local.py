2ed662ea1d37a6b6b7d9486d815b2afec3a80454
Jeff Hutchison <jeff@jeffhutchison.com>
2025-03-01 14:42:17 -0500

    Remove browser reload dev tool

--- puka/settings/local.py
+++ puka/settings/local.py
@@ -13,12 +13,10 @@ DEBUG = True
 
 INSTALLED_APPS += [  # noqa: F405
     "debug_toolbar",
-    "django_browser_reload",
 ]
 
 MIDDLEWARE += [  # noqa: F405
     "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 INTERNAL_IPS = ["127.0.0.1"]
