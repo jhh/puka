42a3ea7fa69be2c21b9894d6ab383ad396aa878b
Jeff Hutchison <jeff@jeffhutchison.com>
2025-03-26 08:21:38 -0400

    Divide integration test into subtests

--- nix/checks/puka-integration-tests.nix
+++ nix/checks/puka-integration-tests.nix
@@ -70,104 +70,103 @@ pkgs.nixosTest {
       # check for missing static files
       machine.succeed(f"wget -nv --level=1 --spider --recursive {login_url}")
 
-      # create a superuser
-      username = "username"
-      password = "password"
+      with subtest("create superuser account"):
+        username = "username"
+        password = "password"
+        machine.succeed(f"""
+            sudo -u puka env \
+            DJANGO_SETTINGS_MODULE=puka.settings.production \
+            SECRET_KEY=test \
+            DJANGO_SUPERUSER_PASSWORD='{password}' \
+            ${venv}/bin/puka-manage createsuperuser --no-input --username='{username}' --email=nobody@j3ff.io
+            """
+        )
 
-      machine.succeed(f"""
-        sudo -u puka env \
-        DJANGO_SETTINGS_MODULE=puka.settings.production \
-        SECRET_KEY=test \
-        DJANGO_SUPERUSER_PASSWORD='{password}' \
-        ${venv}/bin/puka-manage createsuperuser --no-input --username='{username}' --email=nobody@j3ff.io
-        """
-      )
+        # log in as superuser
+        csrf_token = machine.succeed(f"grep csrftoken {cookie_jar_path} | cut --fields=7").rstrip()
+        machine.succeed(f"""
+            {curl} \
+            --data 'csrfmiddlewaretoken={csrf_token}' \
+            --data 'username={username}' \
+            --data 'password={password}' \
+            {login_url}
+            """
+        )
 
-      # log in as superuser
-      csrf_token = machine.succeed(f"grep csrftoken {cookie_jar_path} | cut --fields=7").rstrip()
-      machine.succeed(f"""
-        {curl} \
-        --data 'csrfmiddlewaretoken={csrf_token}' \
-        --data 'username={username}' \
-        --data 'password={password}' \
-        {login_url}
-        """
-      )
+      with subtest("create a bookmark"):
+        # test that main bookmarks list is available
+        assert "New Bookmark" in machine.succeed(f"{curl} --location {base_url}"), "T001"
 
-      # test that main bookmarks list is available
-      assert "New Bookmark" in machine.succeed(f"{curl} --location {base_url}"), "T001"
+        # create a new bookmark
 
-      # create a new bookmark
+        title = generate_random_string(50)
+        csrf_token = machine.succeed(f"grep csrftoken {cookie_jar_path} | cut --fields=7").rstrip()
+        machine.succeed(f"""
+            {curl} -X POST \
+            --data 'csrfmiddlewaretoken={csrf_token}' \
+            --data 'title={title}' \
+            --data 'description=' \
+            --data-urlencode 'url=http://example.com' \
+            --data 'tags=foobar' \
+            --data 'active=on' \
+            {base_url}/bookmarks/new/
+            """
+        )
 
-      title = generate_random_string(50)
-      csrf_token = machine.succeed(f"grep csrftoken {cookie_jar_path} | cut --fields=7").rstrip()
-      machine.succeed(f"""
-        {curl} -X POST \
-        --data 'csrfmiddlewaretoken={csrf_token}' \
-        --data 'title={title}' \
-        --data 'description=' \
-        --data-urlencode 'url=http://example.com' \
-        --data 'tags=foobar' \
-        --data 'active=on' \
-        {base_url}/bookmarks/new/
-        """
-      )
+        # check for this new bookmark in main bookmark list
+        output = machine.succeed(f"{curl} --location {base_url}")
+        assert title in output, "T002"
+        assert "http://example.com" in output, "T003"
+        assert "foobar" in output, "T004"
 
-      # check for this new bookmark in main bookmark list
-      output = machine.succeed(f"{curl} --location {base_url}")
-      assert title in output, "T002"
-      assert "http://example.com" in output, "T003"
-      assert "foobar" in output, "T004"
+      with subtest("seed locations"):
+        machine.succeed("""
+            sudo -u puka env \
+            DJANGO_SETTINGS_MODULE=puka.settings.production \
+            SECRET_KEY=test \
+            ${venv}/bin/puka-manage seed_locations
+            """
+        )
 
-      # seed locations
-      machine.succeed("""
-        sudo -u puka env \
-        DJANGO_SETTINGS_MODULE=puka.settings.production \
-        SECRET_KEY=test \
-        ${venv}/bin/puka-manage seed_locations
-        """
-      )
+        location_code = "S-D01-08"
 
-      location_code = "S-D01-08"
+        # check for this new location in main locations list
+        output = machine.succeed(f"{curl} --location {base_url}/stuff/location/23/")
+        assert location_code in output, "T005"
 
-      # check for this new bookmark in main locations list
-      output = machine.succeed(f"{curl} --location {base_url}/stuff/location/23/")
-      assert location_code in output, "T005"
+      with subtest("create a new item"):
+        name = generate_random_string(50)
+        notes = generate_random_string(50)
+        tags = f"{generate_random_string(3)} {generate_random_string(5)} {generate_random_string(7)}"
+        reorder_level = random.randint(0, 100)
+        location_code = "S-A01"
+        quantity = random.randint(0, 100)
+        bookmark_url = f"https://example.com/{generate_random_string(10)}"
+        csrf_token = machine.succeed(f"grep csrftoken {cookie_jar_path} | cut --fields=7").rstrip()
+        machine.succeed(f"""
+            {curl} -X POST \
+            --data 'csrfmiddlewaretoken={csrf_token}' \
+            --data 'name={name}' \
+            --data 'notes={notes}' \
+            --data 'tags={tags}' \
+            --data 'reorder_level={reorder_level}' \
+            --data 'location_code={location_code}' \
+            --data 'quantity={quantity}' \
+            --data 'bookmark_url={bookmark_url}' \
+            {base_url}/stuff/item/new/
+            """
+        )
 
-      # create a new item
-
-      name = generate_random_string(50)
-      notes = generate_random_string(50)
-      tags = f"{generate_random_string(3)} {generate_random_string(5)} {generate_random_string(7)}"
-      reorder_level = random.randint(0, 100)
-      location_code = "S-A01"
-      quantity = random.randint(0, 100)
-      bookmark_url = f"https://example.com/{generate_random_string(10)}"
-      csrf_token = machine.succeed(f"grep csrftoken {cookie_jar_path} | cut --fields=7").rstrip()
-      machine.succeed(f"""
-        {curl} -X POST \
-        --data 'csrfmiddlewaretoken={csrf_token}' \
-        --data 'name={name}' \
-        --data 'notes={notes}' \
-        --data 'tags={tags}' \
-        --data 'reorder_level={reorder_level}' \
-        --data 'location_code={location_code}' \
-        --data 'quantity={quantity}' \
-        --data 'bookmark_url={bookmark_url}' \
-        {base_url}/stuff/item/new/
-        """
-      )
-
-      # check for this new item in item detail page
-      output = machine.succeed(f"{curl} --location {base_url}/stuff/1/")
-      assert name in output, "T006"
-      assert notes in output, "T007"
-      for tag in tags.split():
-          assert tag in output, f"T008-{tag}"
-      assert str(reorder_level) in output, "T009"
-      assert location_code in output, "T010"
-      assert str(quantity) in output, "T011"
-      assert bookmark_url in output, "T012"
+        # check for this new item in item detail page
+        output = machine.succeed(f"{curl} --location {base_url}/stuff/1/")
+        assert name in output, "T006"
+        assert notes in output, "T007"
+        for tag in tags.split():
+            assert tag in output, f"T008-{tag}"
+        assert str(reorder_level) in output, "T009"
+        assert location_code in output, "T010"
+        assert str(quantity) in output, "T011"
+        assert bookmark_url in output, "T012"
 
     '';
 }
