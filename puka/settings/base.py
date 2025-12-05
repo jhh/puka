71102816da18459207c7ae345b4edfa9e9b85710
Jeff Hutchison <jeff@jeffhutchison.com>
2025-12-05 07:40:51 -0500

    Update to Django 6.0

--- puka/settings/base.py
+++ puka/settings/base.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.postgres",
     "django.contrib.staticfiles",
     "django_htmx",
     "template_partials",
