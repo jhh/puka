1b1165aad39970eb90eee05489c351674bccf6f8
Jeff Hutchison <jeff@jeffhutchison.com>
2026-01-06 14:36:48 -0700

    Upgraded "Hello, World" to web app

--- src/puka/main.clj
+++ src/puka/main.clj
@@ -1,12 +1,18 @@
 (ns puka.main
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(def app
+  (ring/ring-handler
+   (ring/router
+    ["/" {:get {:handler (fn [_]
+                           {:status 200
+                            :headers {"Content-Type" "text/plain"}
+                            :body "Hello, World"})}}])
+   (ring/create-default-handler
+    {:not-found (constantly {:status 404 :body "Not found"})})))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+(defn -main [& _args]
+  (println "Server starting on http://localhost:3000")
+  (jetty/run-jetty app {:port 3000 :join? false}))
