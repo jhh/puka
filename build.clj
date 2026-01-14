a96bddaec3d556f59a300fb39a0e372a72e900fe
Jeff Hutchison <jeff@jeffhutchison.com>
2026-01-14 06:41:30 -0700

    Add babashka tasks

--- build.clj
+++ build.clj
@@ -2,20 +2,10 @@
   (:refer-clojure :exclude [test])
   (:require [clojure.tools.build.api :as b]))
 
-(def lib 'net.clojars.puka/main)
+(def lib 'puka)
 (def version "0.1.0-SNAPSHOT")
 (def main 'puka.main)
 (def class-dir "target/classes")
-
-(defn test "Run all the tests." [opts]
-  (let [basis    (b/create-basis {:aliases [:test]})
-        cmds     (b/java-command
-                  {:basis     basis
-                   :main      'clojure.main
-                   :main-args ["-m" "cognitect.test-runner"]})
-        {:keys [exit]} (b/process cmds)]
-    (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
-  opts)
 
 (defn- uber-opts [opts]
   (assoc opts
@@ -26,8 +16,7 @@
          :src-dirs ["src"]
          :ns-compile [main]))
 
-(defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
-  (test opts)
+(defn uber "Build the uberjar." [opts]
   (b/delete {:path "target"})
   (let [opts (uber-opts opts)]
     (println "\nCopying source...")
