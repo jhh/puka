8b762d33b04bb763125022773a782f494b3d4b62
Jeff Hutchison <jeff@jeffhutchison.com>
2026-01-14 15:01:29 -0700

    Render a bookmark to html

---
+++ src/puka/layouts/bookmark/list.clj
@@ -0,0 +1,60 @@
+(ns puka.layouts.bookmark.list
+  (:require [borkdude.html :refer [html]]
+            [clojure.string :as string])
+  (:import [java.time ZoneId]
+           [java.time.format DateTimeFormatter]
+           [java.util Locale]))
+
+(def ^:private date-formatter
+  (DateTimeFormatter/ofPattern "MMMM yyyy" Locale/ENGLISH))
+
+(defn- format-inst
+  [inst]
+  (string/lower-case
+   (.format date-formatter
+            (.atZone (.toInstant inst) (ZoneId/systemDefault)))))
+
+(defn tag->html
+  [{:keys [name slug]}]
+  (let [uri (str "?tags=" slug)
+        href {:href uri :hx-get uri}]
+    (html [:li {:class "inline text-sm text-red-700"}
+           [:a {:hx-target "#id_content" :& href} name]])))
+
+(defn bookmark->html
+  [{:keys [id title description url created active tags]}]
+  (let [href {:href url}
+        edit-hx-get {:hx-get (str "/bookmarks/" id "/edit/")}]
+    (html [:li {:class "p-4"}
+           [:article
+            [:h2 {:class "pb-2 text-base font-medium text-slate-800"}
+             [:a {:target "_blank", :rel "noreferrer" :& href} title]
+             (when-not active
+               #html [:span {:class "text-sm text-red-700"} "(inactive)"])]
+            [:p {:class "text-sm text-slate-600"} description]
+            [:ul {:role "list", :class "mt-4"}
+             (html (map tag->html tags))]
+            [:div {:class "text-xs text-slate-400"}
+             (format-inst created)
+             [:button {:type "button"
+                       :class "ml-3"
+                       :hx-target "#id_content"
+                       :hx-push-url "true"
+                       :& edit-hx-get} "edit"]]]])))
+
+(comment
+  (str (tag->html {:id 1 :name "Foo" :slug "foo"}))
+
+  (def bm
+    {:id 3960,
+     :title "Awesome",
+     :url "https://github.com/sindresorhus/awesome",
+     :description
+     "😎 Awesome lists about all kinds of interesting topics. Awesome number of stars.",
+     :created #inst "2024-12-25T21:27:52.359000000-00:00",
+     :active false,
+     :tags [{:id 234, :name "github", :slug "github"}]})
+
+  (str (bookmark->html bm))
+  ;
+  )
---
+++ test/puka/layouts/bookmark/list_test.clj
@@ -0,0 +1,30 @@
+(ns puka.layouts.bookmark.list-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [puka.layouts.bookmark.list :as sut]))
+
+(deftest tag-test
+  (testing "renders a tag list item with correct links and name"
+    (let [sample-tag {:name "Clojure" :slug "clojure"}
+          result (str (sut/tag->html sample-tag))]
+      (is (re-find #"href=\"\?tags=clojure\"" result))
+      (is (re-find #"hx-get=\"\?tags=clojure\"" result))
+      (is (re-find #">Clojure</a>" result)))))
+
+(deftest bookmark->html-test
+  (testing "renders a bookmark with correct fields"
+    (let [bm {:id 123
+              :title "My Title"
+              :description "My Description"
+              :url "https://example.com"
+              :created #inst "2026-01-14T12:00:00Z"
+              :active false
+              :tags [{:name "tag1" :slug "tag1"}]}
+          result (str (sut/bookmark->html bm))]
+      (is (re-find #"href=\"https://example.com\"" result))
+      (is (re-find #"My Title" result))
+      (is (re-find #"My Description" result))
+      (is (re-find #"\(inactive\)" result))
+      (is (re-find #"january 2026" result))
+      (is (re-find #"hx-get=\"/bookmarks/123/edit/\"" result))
+      (is (re-find #">tag1</a>" result)))))
+
