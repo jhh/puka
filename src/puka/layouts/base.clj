ae026f48d09b2f4706978ae4dcee62f26f9e49d8
Jeff Hutchison <jeff@jeffhutchison.com>
2026-01-16 14:00:55 -0700

    Format page body

--- src/puka/layouts/base.clj
+++ src/puka/layouts/base.clj
@@ -22,10 +22,11 @@
          content #html [:p [:strong "No content available!"]]}}]
   (html [:<>
          [:$ "<!DOCTYPE html>"]
-         [:html {:lang "en"}
+         [:html {:class "h-full bg-gray-100" :lang "en"}
           (#'header title)
-          [:body
-           [:main
-            (if (string? content)
-              #html [:p content]
-              content)]]]]))
+          [:body.h-full
+           [:main {:class "py-10 lg:pl-72"}
+            [:div#id_content {:class "px-4 sm:px-6 lg:px-8"}
+             (if (string? content)
+               #html [:p content]
+               content)]]]]]))
