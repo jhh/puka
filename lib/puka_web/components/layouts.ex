a688ece0f7586e6de0b67169c230dc4dcbe844bd
Jeff Hutchison <jeff@jeffhutchison.com>
2026-02-14 11:05:58 -0700

    style: add Puka logo to header

--- lib/puka_web/components/layouts.ex
+++ lib/puka_web/components/layouts.ex
@@ -38,8 +38,7 @@ defmodule PukaWeb.Layouts do
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+          <img src={~p"/images/puka-logo.svg"} width="180" />
         </a>
       </div>
       <div class="flex-none">
